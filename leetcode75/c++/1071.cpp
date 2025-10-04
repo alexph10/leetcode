4867445673d800b2ecc847d231b173ac45a15fb2
User <user@example.com>
2025-10-04 00:53:24 -0700

    fixed code formatting

--- leetcode75/c++/1071.cpp
+++ leetcode75/c++/1071.cpp
@@ -1,14 +1,14 @@
 #include <string>
 
-
 using namespace std;
 
 class Solution {
 public:
   string gcdOfString(string str1, string str2) {
-    if (str1 + str2 != str2 + str1)
+    if (str1 + str2 != str2 + str1) {
       return "";
-    size_t g = std::gcd(str1.size()), str2.size());
+    }
+    size_t g = std::gcd(str1.size(), str2.size());
     return str1.substr(0,g);
   }
 };
--- leetcode75/c++/1480.cpp
+++ leetcode75/c++/1480.cpp
@@ -6,11 +6,11 @@ class Solution {
 public:
   vector<int> runningSum(vector<int> &nums) {
     vector<int> ans;
-    ans.reserve(nums.Size());
+    ans.reserve(nums.size());
     int curr = 0;
     for (int x : nums) {
       curr += x;
-      ans.push_bacK(curr);
+      ans.push_back(curr);
     }
     return ans;
   }
